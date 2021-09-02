99cb071f220925083fc25b6db30c0c67ea715182
Maksim <rsincara@mail.ru>
2021-09-02 20:26:23 +0500

    Adding new posts + Date

---
+++ src/main/java/com/example/demoweb/controller/PostsCreateController.java
@@ -0,0 +1,26 @@
+package com.example.demoweb.controller;
+
+import com.example.demoweb.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PostsCreateController {
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
+    public String create(Model model) {
+        model.addAttribute("appName", "Мое супер приложение");
+        return "create";
+    }
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public String doCreate(@ModelAttribute("text") String text) {
+        PostService.create(text);
+        return "redirect:/";    }
+
+
+}
+
+
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -2,18 +2,18 @@ package com.example.demoweb.service;
 
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.Date;
 
 
 @Service
 public class PostService {
+    private static final ArrayList<Post> posts = new ArrayList<>();
     public ArrayList<Post> listAllPosts(){
-        ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post("OOO", new Date()));
-        posts.add(new Post("Moya", new Date()));
-        posts.add(new Post("Oborona!", new Date()));
         return posts;
+    }
+
+    public static void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
